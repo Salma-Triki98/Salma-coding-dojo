9c956d823e3867d50cdabbbd48e3874335d43fb7
Triki Salma <salma.triki98@gmail.com>
2022-12-20 17:34:38 +0100

    CafeBusiness

---
+++ Java/Week1/Day2/Core-Assignment/CafeBusiness/CafeUtil.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class  CafeUtil{
+
+    
+    public int getStreakGoal(int numWeeks){
+    
+    int i=0;
+    int sum = 0;
+    while (i<=numWeeks){
+        sum = sum + i; 
+        i ++;
+    }
+    return sum;
+}
+
+public double getOrderTotal(double[] prices){
+    double sum =0;
+    for (int i=0 ; i<prices.length; i++){
+        sum += prices[i];
+    }
+    return sum;
+}
+
+public void displayMenu(ArrayList<String> menuItems){
+    for (int i=0 ; i<menuItems.size(); i++){
+    String name = menuItems.get(i); 
+    // System.out.println(i + " " + name );
+    System.out.printf("%d %s \n", i, name);
+
+}   
+}
+public void addCustomer(ArrayList<String> customers){
+    System.out.println("Please enter your name:");
+    String userName = System.console().readLine();
+    System.out.println("Hello " + "" + userName);
+    System.out.println("There are "+ customers.size()+" people in front of you" );
+    customers.add(userName);
+    System.out.println(customers);
+}
+}
